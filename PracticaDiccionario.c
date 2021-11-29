3b4a974c1730a7de692c864b9808d31a37723e83
Digitalizer01 <lazarohidalgoismael@gmail.com>
2021-11-29 21:44:21 +0100

    .

--- PracticaDiccionario.c
+++ PracticaDiccionario.c
@@ -10,30 +10,70 @@
 #include <fcntl.h>
 #include <sys/shm.h>
 
-void make_directory(const char *name)
+void make_file(const char *name);
+void inicializar_ficheros();
+
+char LETRAS_ABECEDARIO[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+int tuberia[2];
+
+typedef struct
 {
-#ifdef __linux__
-    mkdir(name, 777);
-#else
-    _mkdir(name);
-#endif
-}
+    int pid;
+    int salida;
+    char letra;
+} datos;
+
+datos *puntero;
 
 int main(int argc, char const *argv[])
 {
+    int estado;
+    int id;
+    int pid;
+    key_t clave;
+    clave = ftok("/bin/ls", 11);
+    id = shmget(clave, sizeof(LETRAS_ABECEDARIO) - 1 * sizeof(datos), 0777 | IPC_CREAT); // El dos indica el número de campos del struct
+    puntero = (datos *)shmat(id, (char *)0, 0);
 
-    char letras_abecedario[26] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'};
-    char str[26];
-    for (size_t i = 0; i < 26; i++)
+    for (int i = 0; i < sizeof(LETRAS_ABECEDARIO); i++)
+    {
+        puntero[i].pid = 0;
+        puntero[i].salida = 0;
+        puntero[i].letra = LETRAS_ABECEDARIO[i];
+        printf("-----\n");
+        printf("puntero[%d].pid=%d \n", i, puntero[i].pid);
+        printf("puntero[%d].salida=%d \n", i, puntero[i].salida);
+        printf("puntero[%d].letra=%c \n", i, puntero[i].letra);
+        printf("-----\n");
+    }
+
+    inicializar_ficheros();
+    return 0;
+}
+
+void make_file(const char *name)
+{
+    // Creamos el directorio letras
+    char dest[] = "mkdir -p letras";
+    system(dest);
+
+    // Creamos el fichero correspondiente a la letra
+    char nombre_fichero[] = "touch letras/Letra_";
+    strcat(nombre_fichero, name);
+    system(nombre_fichero);
+}
+
+void inicializar_ficheros()
+{
+    char str[sizeof(LETRAS_ABECEDARIO)];
+    for (size_t i = 0; i < sizeof(LETRAS_ABECEDARIO); i++)
     {
         // string is a character array
-        str[0] = letras_abecedario[i];
+        str[0] = LETRAS_ABECEDARIO[i];
         str[1] = '\0';
         //string always ends with a null character
 
         //displaying the string
-        make_directory(str);
+        make_file(str);
     }
-
-    return 0;
 }
